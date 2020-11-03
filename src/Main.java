273cb8d3eb3cc92fb7ef5c0f39446214bfecea66
Yakov Marushchak <ymaryshak@gmail.com>
2020-11-03 10:15:14 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        for (int i = 0; i < 100; i++) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
