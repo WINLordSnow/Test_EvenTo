d370385790ed55dcce0b65f77706d91b6b7cc34e
Marushchak Yakov <ymaryshak@bk.ru>
2020-11-13 14:34:26 +0400

    add space

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,27 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        for (int i = 0; i < 100; i++) {
-            if (i % 2 == 0) {
-                System.out.print(i + " ");
+       int[] nums = {-1,0,1,2,-1,-4};
+
+        Map<Integer, Integer> map = new HashMap();
+        List<List<Integer>> lists = new ArrayList<>();
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            map.put(nums[i], 0);
+            for (int j = i + 1; j < nums.length -1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0 & !map.containsKey(nums[k])) {
+                            lists.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    }
+                }
             }
+        }
+        for (List<Integer> i : lists) {
+            i.forEach(n -> System.out.print(n + " "));
+            System.out.println();
+
         }
     }
 }
